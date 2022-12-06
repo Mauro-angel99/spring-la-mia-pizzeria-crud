7134a337f38d646d7969537417ea8fcdf107e072
mauro-angel99 <angelottimauro@gmail.com>
2022-12-06 12:03:45 +0100

    first commit

---
+++ src/main/java/org/generation/italy/demo/SpringLaMiaPizzeriaApplication.java
@@ -0,0 +1,43 @@
+package org.generation.italy.demo;
+
+import java.util.List;
+
+import org.generation.italy.demo.pojo.Pizza;
+import org.generation.italy.demo.serv.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private PizzaService pizzaService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		// INSERIMENTO
+		
+		Pizza p1 = new Pizza("margherita", "mozzarella e pomodoro", 5);
+		Pizza p2 = new Pizza("rossini", "mozzarella, pomodoro, uovo e majonese", 7);
+		Pizza p3 = new Pizza("salsiccia", "mozzarella, pomodoro e salsiccia", 6);
+		Pizza p4 = new Pizza("ciglista", "base bianca", 5);
+		
+		pizzaService.save(p1);
+		pizzaService.save(p2);
+		pizzaService.save(p3);
+		pizzaService.save(p4);
+		
+		// LETTURA
+		
+		List<Pizza> pizzas = pizzaService.findAll();
+		System.out.println(pizzas);
+	}
+
+}
